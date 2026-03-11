22101ce7358aef2eeed0e8fee39d1a5f38d16785
Dylan Eskew <dev.dylan@eskew.dev>
2026-03-10 18:28:28 -0700

    lpc1114-rt: Add port1 support and improvements
    Can now do operations with both Port0 and Port1 of the gpio interfaces,
    which came with some implementation improvements.
    
    Signed-off-by: Dylan Eskew <dev.dylan@eskew.dev>

--- lpc1114-rt/src/gpio.rs
+++ lpc1114-rt/src/gpio.rs
@@ -33,34 +33,56 @@ pub struct GpioPin<Po: Port, Pi: Pin> {
     _port: Po,
     _pin: Pi,
     pin_num: u8,
+    gpio_port: GpioPort<Po>,
 }
 
 impl GpioPin<NoPort, NoPin> {
     #[inline(always)]
     pub fn new() -> GpioPin<NoPort, NoPin> {
-        GpioPin { _port: NoPort, _pin: NoPin, pin_num: 0xFF }
+        GpioPin {
+            _port: NoPort,
+            _pin: NoPin,
+            pin_num: 0xFF,
+            gpio_port: GpioPort {
+                addr: 0x0000_0000,
+                _port: NoPort,
+            },
+        }
     }
 }
 
 macro_rules! into_port {
-    ($func:ident, $port:ident) => {
+    ($func:ident, $port:ident, $addr:literal) => {
         impl GpioPin<NoPort, NoPin> {
             #[inline(always)]
             pub fn $func(self) -> GpioPin<$port, NoPin> {
-                GpioPin { _port: $port, _pin: NoPin, pin_num: 0xFF }
+                GpioPin {
+                    _port: $port,
+                    _pin: NoPin,
+                    pin_num: 0xFF,
+                    gpio_port: GpioPort {
+                        addr: $addr,
+                        _port: $port,
+                    },
+                }
             }
         }
     };
 }
-into_port!(into_port0, Port0);
-into_port!(into_port1, Port1);
+into_port!(into_port0, Port0, 0x5000_0000);
+into_port!(into_port1, Port1, 0x5001_0000);
 
 macro_rules! into_pin {
     ($func:ident, $port:ident, $pin:ident, $pin_num:literal) => {
         impl GpioPin<$port, NoPin> {
             #[inline(always)]
             pub fn $func(self) -> GpioPin<$port, $pin> {
-                GpioPin { _port: $port, _pin: $pin, pin_num: $pin_num }
+                GpioPin {
+                    _port: $port,
+                    _pin: $pin,
+                    pin_num: $pin_num,
+                    gpio_port: self.gpio_port,
+                }
             }
         }
     };
@@ -93,15 +115,15 @@ pub struct GpioPort<P: Port> {
     addr: u32,
     _port: P,
 }
-//reg: &'static mut GpioRegister 
+//reg: &'static mut GpioRegister
 
 impl<P: Port> GpioPort<P> {
     #[inline(always)]
     fn grab_port0() -> GpioPort<Port0> {
-       GpioPort {
-           addr: 0x5000_0000,
-           _port: Port0,
-       }
+        GpioPort {
+            addr: 0x5000_0000,
+            _port: Port0,
+        }
     }
 
     #[inline(always)]
@@ -113,22 +135,33 @@ impl<P: Port> GpioPort<P> {
     }
 }
 
-macro_rules! port0_reg {
-    () => {
-        unsafe { &mut *(Self::grab_port0().addr as *mut GpioRegister) }
-    };
-}
-macro_rules! port1_reg {
-    () => {
-        unsafe { &mut *(Self::grab_port1().addr as *mut GpioRegister) }
+macro_rules! port_reg {
+    ($pin:ident) => {
+        unsafe { &mut *($pin.gpio_port.addr as *mut GpioRegister) }
     };
 }
 
 impl GpioPort<Port0> {
     #[inline(always)]
+    pub fn set_data_high<P: Pin>(pin: &GpioPin<Port0, P>) {
+        unsafe {
+            let reg = port_reg!(pin);
+            write_volatile(&mut reg.data[1 << pin.pin_num], 0xFFFF);
+        }
+    }
+
+    #[inline(always)]
+    pub fn set_data_low<P: Pin>(pin: &GpioPin<Port0, P>) {
+        unsafe {
+            let reg = port_reg!(pin);
+            write_volatile(&mut reg.data[1 << pin.pin_num], 0x0000);
+        }
+    }
+
+    #[inline(always)]
     pub fn set_dir_input<P: Pin>(pin: &GpioPin<Port0, P>) {
         unsafe {
-            let reg = port0_reg!();
+            let reg = port_reg!(pin);
             let dir = read_volatile(&mut reg.dir);
             write_volatile(&mut reg.dir, dir & !(1 << pin.pin_num));
         }
@@ -137,25 +170,99 @@ impl GpioPort<Port0> {
     #[inline(always)]
     pub fn set_dir_output<P: Pin>(pin: &GpioPin<Port0, P>) {
         unsafe {
-            let reg = port0_reg!();
+            let reg = port_reg!(pin);
             let dir = read_volatile(&mut reg.dir);
             write_volatile(&mut reg.dir, dir | (1 << pin.pin_num));
         }
     }
 
     #[inline(always)]
-    pub fn set_data_high<P: Pin>(pin: &GpioPin<Port0, P>) {
+    pub fn set_is_edge<P: Pin>(pin: &GpioPin<Port0, P>) {
         unsafe {
-            let reg = port0_reg!();
-            write_volatile(&mut reg.data[1 << pin.pin_num], 0xFFFF);
+            let reg = port_reg!(pin);
+            let is = read_volatile(&mut reg.dir);
+            write_volatile(&mut reg.is, is & !(1 << pin.pin_num));
         }
     }
 
     #[inline(always)]
-    pub fn set_data_low<P: Pin>(pin: &GpioPin<Port0, P>) {
+    pub fn set_is_level<P: Pin>(pin: &GpioPin<Port0, P>) {
         unsafe {
-            let reg = port0_reg!();
-            write_volatile(&mut reg.data[1 << pin.pin_num], 0x0000);
+            let reg = port_reg!(pin);
+            let is = read_volatile(&mut reg.dir);
+            write_volatile(&mut reg.is, is | (1 << pin.pin_num));
+        }
+    }
+
+    #[inline(always)]
+    pub fn set_ibe_event_reg<P: Pin>(pin: &GpioPin<Port0, P>) {
+        unsafe {
+            let reg = port_reg!(pin);
+            let ibe = read_volatile(&mut reg.ibe);
+            write_volatile(&mut reg.ibe, ibe & !(1 << pin.pin_num));
+        }
+    }
+
+    #[inline(always)]
+    pub fn set_ibe_both_edges<P: Pin>(pin: &GpioPin<Port0, P>) {
+        unsafe {
+            let reg = port_reg!(pin);
+            let ibe = read_volatile(&mut reg.ibe);
+            write_volatile(&mut reg.ibe, ibe | (1 << pin.pin_num));
+        }
+    }
+
+    #[inline(always)]
+    pub fn set_iev_low<P: Pin>(pin: &GpioPin<Port0, P>) {
+        unsafe {
+            let reg = port_reg!(pin);
+            let iev = read_volatile(&mut reg.iev);
+            write_volatile(&mut reg.iev, iev & !(1 << pin.pin_num));
+        }
+    }
+
+    #[inline(always)]
+    pub fn set_iev_high<P: Pin>(pin: &GpioPin<Port0, P>) {
+        unsafe {
+            let reg = port_reg!(pin);
+            let iev = read_volatile(&mut reg.iev);
+            write_volatile(&mut reg.iev, iev | (1 << pin.pin_num));
+        }
+    }
+
+    #[inline(always)]
+    pub fn set_ie_disable<P: Pin>(pin: &GpioPin<Port0, P>) {
+        unsafe {
+            let reg = port_reg!(pin);
+            let ie = read_volatile(&mut reg.ie);
+            write_volatile(&mut reg.ie, ie & !(1 << pin.pin_num));
+        }
+    }
+
+    #[inline(always)]
+    pub fn set_ie_enable<P: Pin>(pin: &GpioPin<Port0, P>) {
+        unsafe {
+            let reg = port_reg!(pin);
+            let ie = read_volatile(&mut reg.ie);
+            write_volatile(&mut reg.ie, ie | (1 << pin.pin_num));
+        }
+    }
+
+    #[inline(always)]
+    pub fn get_ris<P: Pin>(pin: &GpioPin<Port0, P>) -> u32 {
+        unsafe {
+            let reg = port_reg!(pin);
+            let ris = read_volatile(&mut reg.ris);
+            (ris & (1 << pin.pin_num)) >> pin.pin_num
+        }
+    }
+
+    #[inline(always)]
+    pub fn get_mis<P: Pin>(pin: &GpioPin<Port0, P>) -> u32 {
+        unsafe {
+            let reg = port_reg!(pin);
+            let mis = read_volatile(&mut reg.mis);
+            (mis & (1 << pin.pin_num)) >> pin.pin_num
         }
     }
 }
@@ -163,14 +270,14 @@ impl GpioPort<Port0> {
 #[repr(C)]
 #[repr(align(4))]
 struct GpioRegister {
-    data: [u32; 1 << 12],
+    data: [u32; 1 << 12], // RW
     rsv0: [u32; 1 << 12],
-    dir: u32,
-    is: u32,
-    be: u32,
-    ev: u32,
-    e: u32,
-    ris: u32,
-    mis: u32,
-    ic: u32,
+    dir: u32, // RW
+    is: u32,  // RW
+    ibe: u32, // RW
+    iev: u32, // RW
+    ie: u32,  // RW
+    ris: u32, // RO
+    mis: u32, // RO
+    ic: u32,  // WO
 }
